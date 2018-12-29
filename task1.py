114118a18ca64415d46cfe9491db3f212267efb5
wstereshchenko <46228375+wstereshchenko@users.noreply.github.com>
2018-12-29 10:14:28 +0300

    version 1

---
+++ task1.py
@@ -0,0 +1,11 @@
+def create_dictionary(sheet_1, sheet_2):
+    dictionary = {}
+    if len(sheet_1) > len(sheet_2):
+        for i in range(len(sheet_1) - len(sheet_2)):
+            sheet_2.append(None)
+
+    for i in range(len(sheet_1)):
+        # dictionary[sheet_1[i]] = sheet_2[i]
+        dictionary = dict(zip(sheet_1, sheet_2))
+    return dictionary
+
